b8815db523cf5c32f0cf6dc38c07bb06a4bba649
Jocelyn <50944289+JocelynCC@users.noreply.github.com>
2019-05-23 16:15:35 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myImage = document.querySelector('img');
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/logo.jpg');
+    } else {
+        myImage.setAttribute('src','images/firefox-icon.png');
+    }
+};
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+myButton.onclick = function(){
+    setUserName();
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+function setUserName() {
+    var myName = prompt('Please enter your name.','');
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
